dc760f57d95b9039631b392f916b69120d653a32
XPloadED <bastiwalz97@web.de>
2021-07-01 17:39:53 +0200

    Structure Cleanup
    ~ File structure cleaned up
    + fixed AluhutController check for NPC spawning (< to >)
    + added CursorUI Layer
    + changed Canvas Camera Render from Screen Space - Overlay to Sorting Layer based method (Render Mode: Screen Space Camera)
    + changed overall Sorting Layer of used prefabs to Prefabs
    
    Co-Authored-By: ppaetsch <85558943+ppaetsch@users.noreply.github.com>

--- Abschlussprojekt_MMP/Assets/Game Level/scripts/AluhutController.cs
+++ Abschlussprojekt_MMP/Assets/Game Level/scripts/AluhutController.cs
@@ -19,13 +19,14 @@ public class AluhutController : MonoBehaviour
     void Update()
     {
         counter += Time.deltaTime;
-        if (counter <= spawnTime )
+        if (counter >= spawnTime )
         {
             counter = 0.0f;
-            Vector2 spawnPosition = new Vector2(Random.Range(-screenBounds.x, screenBounds.x*1.01f), Random.Range(-screenBounds.y * 0.5f, - screenBounds.x * 0.04f));
+            Vector2 spawnPosition = new Vector2(Random.Range(-screenBounds.x, screenBounds.x*1.01f), Random.Range(-screenBounds.y * 0.5f, - screenBounds.y * 0.04f));
             
             GameObject aluhutPerson = Instantiate<GameObject>(aluhutPrefabs[Random.Range(0, aluhutPrefabs.Count)]);
             aluhutPerson.transform.position = spawnPosition;
+            //aluhutPerson.hat.GetComponent<SpriteRenderer>().sortingLayerID = SortingLayer.NameToID("Prefabs");
         }
 
     }
