ff0d03588d44e634ba91e9972eb891d40e6311e5
XPloadED <bastiwalz97@web.de>
2021-07-17 16:50:38 +0200

    Basic NPC movement logic & animations
    ~ changed location for relevant NPC prefabs to Sprites/NPCs (currently one default one + one intended for easteregg/special interaction)
    + rigged and animated both npc prefabs
    - cleaned up npcScript (which should probably be moved to cursorScript as it kind of only handls the hit detection
    +added tinfoilhat assets
    + added npcMovement.cs script for basic NPC movement around the level
    + run.anim, idle.anim, rare_idle.anim, rare_panic.anim

--- Abschlussprojekt_MMP/Assets/Game Level/scripts/npcScript.cs
+++ Abschlussprojekt_MMP/Assets/Game Level/scripts/npcScript.cs
@@ -5,27 +5,12 @@ using UnityEngine.EventSystems;
 
 public class npcScript : MonoBehaviour
 {
-    //**
-    //[SerializeField] private float speed = 0.2f; 
-   // private Vector2 screenBounds;
-    private float width, height, xmin, xmax, ymin, ymax;
-    Vector3 pos, mousePosition;
     public guiBehaviour guiScript;
     AudioSource bonusSound;
-
-    //private GameObject greenHat2;
-
-
-
-    //RectTransform hitBox;
-
     // Start is called before the first frame update
     void Start()
     {
         bonusSound = GetComponent<AudioSource>();
-
-
-        //GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0.0f);
     }
 
     // Update is called once per frame
@@ -33,11 +18,8 @@ public class npcScript : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-          
-
             //Debug.Log("Click, shooting ray");
             ShootRay();
-
         }
     }
 
@@ -57,24 +39,4 @@ public class npcScript : MonoBehaviour
             }
         }
     }
-/*
-    void detectHit()
-    {
-        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        width = hitBox.rect.width;
-        height = hitBox.rect.height;
-        width = hitBox.transform.localScale.x;
-        height = hitBox.transform.localScale.y;
-        pos = hitBox.position;
-        xmax = pos.x + width;
-        xmin = pos.x - width;
-        ymax = pos.y + height;
-        ymin = pos.y - height;
-        if (mousePosition.x < xmax && mousePosition.x > xmin && mousePosition.y < ymax && mousePosition.y > ymin)
-            {
-                GameObject.Destroy(greenHat2);
-                guiScript.manipulateScore(100);
-            }
-    }
-*/
 }
