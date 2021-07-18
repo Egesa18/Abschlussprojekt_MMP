332bf9f6748953e335d48ebe632f33f69c6953c7
XPloadED <bastiwalz97@web.de>
2021-07-18 10:16:37 +0200

    Big level fidelity update
    + greatly increased grass freqeuency
    + added mid level Buildings, trees and a graveyard intended for a 666 easteregg, including hitcolliders
    + added vertical Camera movement in movementScript
    ~ big changes to the RenderingPosition.cs and npcRenderPositionScript.cs scripts
      ++ the NPCs are now bigger if they are closer to the camera and smaller if they move towards the building complex
      ++ added a toggle alsoScaleSprite which makes it possible to apply uniform scaling to chosen props using that script (in this case I chose to scale the animated grass based on camera distance)

--- Abschlussprojekt_MMP/Assets/Game Level/scripts/Level scripts/RenderPosition.cs
+++ Abschlussprojekt_MMP/Assets/Game Level/scripts/Level scripts/RenderPosition.cs
@@ -8,7 +8,10 @@ public class RenderPosition : MonoBehaviour
     //Used to offset the order in layer based on objects y coordinate 
     [SerializeField] private int sortingOrderOffset = 10000;
     [SerializeField] private bool onlyOnce = false;
+    [SerializeField] private bool alsoScaleSprite = false;
     [SerializeField] private float offset = 0;
+    private float finalOrder;
+    private float scaleFactor = 1f;
     private Renderer myRenderer;
 
 
@@ -21,7 +24,20 @@ public class RenderPosition : MonoBehaviour
     void LateUpdate()
     {
         //sets the specific sorting order in that layer to a common sortingOrderOffset, depending on the where the object is located in the scene (lower y value generally means the object is closer to the camera and is thus rendered first)
-        myRenderer.sortingOrder = (int)(sortingOrderOffset - (transform.position.y-offset)*100);
+        finalOrder = (sortingOrderOffset - (transform.position.y - offset) * 100);
+        
+        if (alsoScaleSprite)
+        {
+            //also takes prescaled sprite into consideration; care, this only works as intended on uniform scales, otherwise we need separate scaleFactors for x,y,z
+            scaleFactor = (finalOrder / sortingOrderOffset) * this.transform.localScale.x;
+            this.gameObject.transform.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+            finalOrder = (sortingOrderOffset - (transform.position.y - offset * scaleFactor) * 100);    //render order also changes depending on the scaleFactor
+            myRenderer.sortingOrder = (int)finalOrder;
+        } else
+        {
+            myRenderer.sortingOrder = (int)finalOrder;
+        }
+
         if (onlyOnce)
         {
             Destroy(this);
--- Abschlussprojekt_MMP/Assets/Game Level/scripts/Level scripts/npcRenderPositionScript.cs
+++ Abschlussprojekt_MMP/Assets/Game Level/scripts/Level scripts/npcRenderPositionScript.cs
@@ -7,7 +7,9 @@ public class npcRenderPositionScript : MonoBehaviour
 {
     //Used to offset the order in layer based on objects y coordinate 
     [SerializeField]
-    private int sortingOrderOffset = 10000;
+    private int sortingOrderOffset = 100;
+    private float finalOrder;
+    private float scaleFactor = 1f;
     private SortingGroup myRenderer;
     private int prefabOffset = 1;
 
@@ -21,6 +23,10 @@ public class npcRenderPositionScript : MonoBehaviour
     {
         //sets the specific sorting order in that layer to a common sortingOrderOffset, depending on the where the object is located in the scene (lower y value generally means the object is closer to the camera and is thus rendered first)
         //the offset is needed to accomodate for the relative y position of the prefabs, which would be center otherwise
-        myRenderer.sortingOrder = (int)(sortingOrderOffset - (transform.position.y - prefabOffset)*100);
+        finalOrder = (sortingOrderOffset - (transform.position.y - prefabOffset) * 100);
+        scaleFactor = finalOrder / sortingOrderOffset;
+        this.gameObject.transform.localScale = new Vector3(scaleFactor,scaleFactor, scaleFactor);
+        finalOrder = (sortingOrderOffset - (transform.position.y - prefabOffset*scaleFactor) * 100);
+        myRenderer.sortingOrder = (int)finalOrder;
     }
 }
