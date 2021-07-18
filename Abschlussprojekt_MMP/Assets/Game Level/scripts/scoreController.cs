629823ce68998b47cec8f275ae9373470dbe3ed4
Egesa18 <66838635+Egesa18@users.noreply.github.com>
2021-07-18 10:49:11 +0200

    commit2

--- Abschlussprojekt_MMP/Assets/Game Level/scripts/scoreController.cs
+++ Abschlussprojekt_MMP/Assets/Game Level/scripts/scoreController.cs
@@ -23,10 +23,8 @@ public class scoreController : MonoBehaviour
         instance = this;
         if (PlayerPrefs.HasKey("highScore"))
         {
-
-
             highscore = PlayerPrefs.GetInt("highScore");
-            scoreboard_highscore_text.text = score.ToString();
+            scoreboard_highscore_text.text = highscore.ToString();
         }
     }
 
@@ -41,7 +39,8 @@ public class scoreController : MonoBehaviour
         score+= increment;
         updateHighscore();
         scoreboard_score_text.text = score.ToString();
-       
+        
+
 
 
 
@@ -52,16 +51,25 @@ public class scoreController : MonoBehaviour
         if (score > highscore)
         {
             highscore = score;
-            scoreboard_highscore_text.text = score.ToString();
+            scoreboard_highscore_text.text = highscore.ToString();
+            Debug.Log(scoreboard_highscore_text.text);
             PlayerPrefs.SetInt("highScore", highscore);
             
-        }
+        } 
+    
     }
     public void ResetScore()
     {
         score = 0;
         scoreboard_score_text.text = score.ToString();
-        scoreboard_highscore_text.text = score.ToString();
+        scoreboard_highscore_text.text = highscore.ToString();
 
+    }
+
+    public void ResetHighScore()
+    {
+        PlayerPrefs.DeleteKey("highScore");
+        highscore = 0;
+        scoreboard_highscore_text.text = highscore.ToString();
     }
 }
