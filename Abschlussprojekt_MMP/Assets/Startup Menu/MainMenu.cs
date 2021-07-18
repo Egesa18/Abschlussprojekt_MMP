08800e87c849c08893b4885748691050eb3968cb
Egesa18 <66838635+Egesa18@users.noreply.github.com>
2021-07-18 17:07:35 +0200

    main menu contection to level 1

--- Abschlussprojekt_MMP/Assets/Startup Menu/MainMenu.cs
+++ Abschlussprojekt_MMP/Assets/Startup Menu/MainMenu.cs
@@ -5,21 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    
+
     // private GameObject backgroundsound;
-    public GameObject backgroundmusic;
+    public GameObject endScreen;
     public void ExitButton()
     {
       
         Application.Quit();
         Debug.Log("You Exited The Game");
-        backgroundmusic.GetComponent<AudioSource>().Stop();        
+    //    backgroundmusic.GetComponent<AudioSource>().Stop();        
 
     }
 public void StartGame()
     {
+        //endScreen.SetActive(false);
         SceneManager.LoadScene("Level1");
         //Cursor.visible = false;
+       
+    }
+
+    public void goHome()
+    {
+      
+        SceneManager.LoadScene("Main Menu");
+        Debug.Log("loading scene...");
+        //Cursor.visible = false;
+        endScreen.SetActive(false);
+       
     }
 
 }
